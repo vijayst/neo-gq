4dd6c00cc47aabee08c9262045641992a70a5a37
vijayst <vijayst@gmail.com>
2019-06-01 14:20:15 +0530

    seeding product data

--- client/seed.js
+++ client/seed.js
@@ -13,17 +13,31 @@ const client = new ApolloClient({
     cache: new InMemoryCache()
 });
 
-client
-    .mutate({
-        mutation: gql(seedmutations)
-    })
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-
-export function seed() {
-    axios.get('https://cupshe.com/products.json?page=3').then(response => {
-        response.data.products.forEach((product, index) => {
-            console.log(index, product.title);
-        });
+axios.get('https://cupshe.com/products.json?page=3').then(response => {
+    response.data.products.forEach(product => {
+        client
+            .mutate({
+                mutation: gql(`
+                        mutation ($id: ID, $title: String, $handle: String, $body_html: String, $published_at: String, $created_at: String, $updated_at: String, $vendor: String, $product_type: String) {
+                            CreateProduct(id: $id, title: $title, handle: $handle, body_html: $body_html, published_at: $published_at, created_at: $created_at, updated_at: $updated_at, vendor: $vendor, product_type: $product_type) {
+                                id,
+                                title
+                            }
+                        }
+                    `),
+                variables: {
+                    id: product.id,
+                    title: product.title,
+                    handle: product.handle,
+                    body_html: product.body_html,
+                    published_at: product.published_at,
+                    created_at: product.created_at,
+                    updated_at: product.updated_at,
+                    vendor: product.vendor,
+                    product_type: product.product_type
+                }
+            })
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
     });
-}
+});
